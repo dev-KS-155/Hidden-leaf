6f91e6375f525e7690e8bf85cf9a350b1b859b2a
N.Karthik Akshaj <akshajnk@gmail.com>
2023-10-15 18:12:44 +0530

    Test PR

--- Client/src/app/page.js
+++ Client/src/app/page.js
@@ -1,11 +1,9 @@
 import Image from 'next/image'
-import Homepage from './Homepage/Homepage'
 
 export default function Home() {
   return (
     <>
-      {/* <h1 className='bg-white text-center text-blue-500'>Testing</h1> */}
-      < Homepage />
+      <h1 className='bg-white text-center text-blue-500'>Testing</h1>
     </>
   )
 }
