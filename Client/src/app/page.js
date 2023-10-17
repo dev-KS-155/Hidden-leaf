3ac53fe6eac86f9f9e329fc0743345b628431383
N.Karthik Akshaj <akshajnk@gmail.com>
2023-10-17 13:37:15 +0530

    Feat: Added navbar and carousel component

--- Client/src/app/page.js
+++ Client/src/app/page.js
@@ -1,9 +1,13 @@
+import NavbarMain from '@/componets/Navbar/Navbar'
+// import Carousel from '@/componets/carousel/Carousel'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
-      <h1 className='bg-white text-center text-blue-500'>Testing</h1>
+      {/* <h1 className='bg-white text-center text-blue-500'>Testing</h1> */}
+      {/* < Carousel /> */}
+      < NavbarMain />
     </>
   )
 }
---
+++ Client/src/componets/carousel/Carousel.jsx
@@ -0,0 +1,50 @@
+"use client"
+
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './Carousel.css';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+import Bus from "../../../public/images/Bus.jpg";
+
+export default function App() {
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+         <SwiperSlide>
+          <img src={Bus.src} alt="Bus" height={Bus.height} width={Bus.width} />
+        </SwiperSlide>
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
