0b74cfded969850d5024cc8e3dba98fd41e94f25
N.Karthik Akshaj <akshajnk@gmail.com>
2023-11-04 19:19:55 +0530

    added many components

--- Client/src/app/page.js
+++ Client/src/app/page.js
@@ -1,13 +1,19 @@
-import NavbarMain from '@/componets/Navbar/Navbar'
-// import Carousel from '@/componets/carousel/Carousel'
-import Image from 'next/image'
+import HomeImage from '@/components/HomeImage/HomeImage'
+import Navbar from '@/components/Navbar/Navbar'
+import Credentials from "@/components/Credentials/Credentials";
+import AboutUs from "@/components/AboutUs/AboutUs"
+import UserProfile from '@/components/UserProfile/UserProfile';
 
 export default function Home() {
   return (
     <>
       {/* <h1 className='bg-white text-center text-blue-500'>Testing</h1> */}
-      {/* < Carousel /> */}
-      < NavbarMain />
+      {/* < Navbar /> */}
+      {/* < HomeImage /> */}
+      {/* < AboutUs /> */}
+      {/* < Credentials /> */}
+      < UserProfile />
+
     </>
   )
 }
